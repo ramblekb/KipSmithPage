8d1c1c84a2e5c82f8513c947f194657693b18016
Kip Bradley Smith <ramblekb@gmail.com>
2021-03-04 23:14:05 -0600

    Update email.php

--- email.php
+++ email.php
@@ -35,7 +35,7 @@ if(isset($_POST['email']) && $_POST['email'] != ''){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Studio BOCA Contact</title>
+    <title>Kip's Email</title>
     <link rel="stylesheet" href="/assets/css/email.css" media="all">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="email.js"></script>
